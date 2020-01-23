08bc383796f51293f9b3451b54f1d6372fd73146
zhangchao17 <373138714@qq.com>
2020-01-23 09:35:24 +0800

    first commit

--- unpackage/dist/dev/mp-weixin/common/vendor.js
+++ unpackage/dist/dev/mp-weixin/common/vendor.js
@@ -737,7 +737,7 @@ function initData(vueOptions, context) {
     try {
       data = data.call(context); // 支持 Vue.prototype 上挂的数据
     } catch (e) {
-      if (Object({"VUE_APP_PLATFORM":"mp-weixin","NODE_ENV":"development","BASE_URL":"/"}).VUE_APP_DEBUG) {
+      if (Object({"NODE_ENV":"development","VUE_APP_PLATFORM":"mp-weixin","BASE_URL":"/"}).VUE_APP_DEBUG) {
         console.warn('根据 Vue 的 data 函数初始化小程序 data 失败，请尽量确保 data 函数中不访问 vm 对象，否则可能影响首次数据渲染速度。', data);
       }
     }
@@ -7065,7 +7065,7 @@ function type(obj) {
 
 function flushCallbacks$1(vm) {
     if (vm.__next_tick_callbacks && vm.__next_tick_callbacks.length) {
-        if (Object({"VUE_APP_PLATFORM":"mp-weixin","NODE_ENV":"development","BASE_URL":"/"}).VUE_APP_DEBUG) {
+        if (Object({"NODE_ENV":"development","VUE_APP_PLATFORM":"mp-weixin","BASE_URL":"/"}).VUE_APP_DEBUG) {
             var mpInstance = vm.$scope;
             console.log('[' + (+new Date) + '][' + (mpInstance.is || mpInstance.route) + '][' + vm._uid +
                 ']:flushCallbacks[' + vm.__next_tick_callbacks.length + ']');
@@ -7086,14 +7086,14 @@ function nextTick$1(vm, cb) {
     //1.nextTick 之前 已 setData 且 setData 还未回调完成
     //2.nextTick 之前存在 render watcher
     if (!vm.__next_tick_pending && !hasRenderWatcher(vm)) {
-        if(Object({"VUE_APP_PLATFORM":"mp-weixin","NODE_ENV":"development","BASE_URL":"/"}).VUE_APP_DEBUG){
+        if(Object({"NODE_ENV":"development","VUE_APP_PLATFORM":"mp-weixin","BASE_URL":"/"}).VUE_APP_DEBUG){
             var mpInstance = vm.$scope;
             console.log('[' + (+new Date) + '][' + (mpInstance.is || mpInstance.route) + '][' + vm._uid +
                 ']:nextVueTick');
         }
         return nextTick(cb, vm)
     }else{
-        if(Object({"VUE_APP_PLATFORM":"mp-weixin","NODE_ENV":"development","BASE_URL":"/"}).VUE_APP_DEBUG){
+        if(Object({"NODE_ENV":"development","VUE_APP_PLATFORM":"mp-weixin","BASE_URL":"/"}).VUE_APP_DEBUG){
             var mpInstance$1 = vm.$scope;
             console.log('[' + (+new Date) + '][' + (mpInstance$1.is || mpInstance$1.route) + '][' + vm._uid +
                 ']:nextMPTick');
@@ -7169,7 +7169,7 @@ var patch = function(oldVnode, vnode) {
     });
     var diffData = diff(data, mpData);
     if (Object.keys(diffData).length) {
-      if (Object({"VUE_APP_PLATFORM":"mp-weixin","NODE_ENV":"development","BASE_URL":"/"}).VUE_APP_DEBUG) {
+      if (Object({"NODE_ENV":"development","VUE_APP_PLATFORM":"mp-weixin","BASE_URL":"/"}).VUE_APP_DEBUG) {
         console.log('[' + (+new Date) + '][' + (mpInstance.is || mpInstance.route) + '][' + this._uid +
           ']差量更新',
           JSON.stringify(diffData));
@@ -7594,9 +7594,9 @@ module.exports = g;
 
 /***/ }),
 /* 4 */
-/*!**************************************!*\
-  !*** C:/code/app/rep_sys/pages.json ***!
-  \**************************************/
+/*!*******************************************!*\
+  !*** /Users/zhangchao/rep_sys/pages.json ***!
+  \*******************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -8501,9 +8501,9 @@ module.exports = {"_from":"@dcloudio/uni-stat@alpha","_id":"@dcloudio/uni-stat@2
 
 /***/ }),
 /* 7 */
-/*!*******************************************************!*\
-  !*** C:/code/app/rep_sys/pages.json?{"type":"style"} ***!
-  \*******************************************************/
+/*!************************************************************!*\
+  !*** /Users/zhangchao/rep_sys/pages.json?{"type":"style"} ***!
+  \************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -8512,9 +8512,9 @@ Object.defineProperty(exports, "__esModule", { value: true });exports.default = 
 
 /***/ }),
 /* 8 */
-/*!******************************************************!*\
-  !*** C:/code/app/rep_sys/pages.json?{"type":"stat"} ***!
-  \******************************************************/
+/*!***********************************************************!*\
+  !*** /Users/zhangchao/rep_sys/pages.json?{"type":"stat"} ***!
+  \***********************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -8654,9 +8654,9 @@ function normalizeComponent (
 /* 19 */,
 /* 20 */,
 /* 21 */
-/*!***************************************!*\
-  !*** C:/code/app/rep_sys/api/user.js ***!
-  \***************************************/
+/*!********************************************!*\
+  !*** /Users/zhangchao/rep_sys/api/user.js ***!
+  \********************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -8683,9 +8683,9 @@ Object.defineProperty(exports, "__esModule", { value: true });exports.default = 
 
 /***/ }),
 /* 22 */
-/*!******************************************************!*\
-  !*** C:/code/app/rep_sys/common/request/js/index.js ***!
-  \******************************************************/
+/*!***********************************************************!*\
+  !*** /Users/zhangchao/rep_sys/common/request/js/index.js ***!
+  \***********************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -8723,9 +8723,9 @@ create;exports.default = _default;
 
 /***/ }),
 /* 23 */
-/*!***********************************************************!*\
-  !*** C:/code/app/rep_sys/common/request/js/core/index.js ***!
-  \***********************************************************/
+/*!****************************************************************!*\
+  !*** /Users/zhangchao/rep_sys/common/request/js/core/index.js ***!
+  \****************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -8870,9 +8870,9 @@ MyRequest;exports.default = _default;
 
 /***/ }),
 /* 24 */
-/*!*****************************************************************!*\
-  !*** C:/code/app/rep_sys/common/request/js/core/interceptor.js ***!
-  \*****************************************************************/
+/*!**********************************************************************!*\
+  !*** /Users/zhangchao/rep_sys/common/request/js/core/interceptor.js ***!
+  \**********************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -8921,9 +8921,9 @@ Interceptor;exports.default = _default;
 
 /***/ }),
 /* 25 */
-/*!*****************************************************************!*\
-  !*** C:/code/app/rep_sys/common/request/js/core/mergeConfig.js ***!
-  \*****************************************************************/
+/*!**********************************************************************!*\
+  !*** /Users/zhangchao/rep_sys/common/request/js/core/mergeConfig.js ***!
+  \**********************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -9008,9 +9008,9 @@ mergeConfig;exports.default = _default;
 
 /***/ }),
 /* 26 */
-/*!******************************************************!*\
-  !*** C:/code/app/rep_sys/common/request/js/tools.js ***!
-  \******************************************************/
+/*!***********************************************************!*\
+  !*** /Users/zhangchao/rep_sys/common/request/js/tools.js ***!
+  \***********************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -9195,9 +9195,9 @@ $;exports.default = _default;
 
 /***/ }),
 /* 27 */
-/*!*************************************************************!*\
-  !*** C:/code/app/rep_sys/common/request/js/core/network.js ***!
-  \*************************************************************/
+/*!******************************************************************!*\
+  !*** /Users/zhangchao/rep_sys/common/request/js/core/network.js ***!
+  \******************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -9281,9 +9281,9 @@ function addTask(config, instance) {
 
 /***/ }),
 /* 28 */
-/*!*******************************************************!*\
-  !*** C:/code/app/rep_sys/common/request/js/config.js ***!
-  \*******************************************************/
+/*!************************************************************!*\
+  !*** /Users/zhangchao/rep_sys/common/request/js/config.js ***!
+  \************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -10246,9 +10246,9 @@ if (hadRuntime) {
 /* 44 */,
 /* 45 */,
 /* 46 */
-/*!*************************************!*\
-  !*** C:/code/app/rep_sys/api/wo.js ***!
-  \*************************************/
+/*!******************************************!*\
+  !*** /Users/zhangchao/rep_sys/api/wo.js ***!
+  \******************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
